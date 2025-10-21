db1e80fd87b76bfb9fda2f70a03abe08f0e508c5
samueldanso <me.samueldanso@gmail.com>
2025-10-21 04:24:57 +0000

    add hero section

---
+++ frontend/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import { HeroSection } from './_components/hero-section'
+
+export default function Home() {
+	return (
+		<div className="relative">
+			<HeroSection />
+		</div>
+	)
+}
--- frontend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Neria AI</h1>
-    </div>
-  );
-}
