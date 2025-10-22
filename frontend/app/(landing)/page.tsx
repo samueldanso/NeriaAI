29766c34f94babf9f9c206bbc1539fdf88fd8c05
samueldanso <me.samueldanso@gmail.com>
2025-10-22 05:19:13 +0000

    improve ui

--- frontend/app/(landing)/page.tsx
+++ frontend/app/(landing)/page.tsx
@@ -1,19 +1,19 @@
-import CTASection from "./_components/cta-section";
-import FeaturesSection from "./_components/features";
-import Footer from "./_components/footer";
-import Header from "./_components/header";
-import { HeroSection } from "./_components/hero-section";
-import IntegrationsSection from "./_components/integrations";
+import CTASection from './_components/cta-section'
+import FeaturesSection from './_components/features'
+import Footer from './_components/footer'
+import Header from './_components/header'
+import { HeroSection } from './_components/hero-section'
+import IntegrationsSection from './_components/integrations'
 
 export default function Home() {
-  return (
-    <main className="container mx-auto px-4 py-8">
-      <Header />
-      <HeroSection />
-      <FeaturesSection />
-      <CTASection />
-      <IntegrationsSection />
-      <Footer />
-    </main>
-  );
+	return (
+		<main className="container mx-auto px-4 py-8">
+			<Header />
+			<HeroSection />
+			<FeaturesSection />
+			<IntegrationsSection />
+			<CTASection />
+			<Footer />
+		</main>
+	)
 }
